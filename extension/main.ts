75fede115f7d9d7299b6972e5c992a03676653fd
wooningeire <22846982+wooningeire@users.noreply.github.com>
2025-04-29 02:12:15 -0400

    feat: toml injection attempt

--- extension/main.ts
+++ extension/main.ts
@@ -1,7 +1,22 @@
-import {parse} from "@eemeli/yaml";
+import {exists} from "jsr:@std/fs/exists";
+import {parse} from "jsr:@eemeli/yaml";
 
-const grammarYaml = new TextDecoder().decode(await Deno.readFile("./syntaxes/hatchery.tmLanguage.yaml"));
-const grammar = parse(grammarYaml);
-const grammarJson = JSON.stringify(grammar);
-await Deno.writeFile("./out/hatchery.tmLanguage.json", new TextEncoder().encode(grammarJson));
 
+if (await exists("./out")) {
+    await Deno.remove("./out", {recursive: true});
+}
+await Deno.mkdir("./out", {recursive: true});
+
+for await (const file of Deno.readDir("./syntaxes")) {
+    if (!file.isFile) continue;
+    if (!file.name.endsWith(".yaml")) continue;
+
+    const grammarYaml = new TextDecoder().decode(await Deno.readFile(`./syntaxes/${file.name}`));
+    const grammar = parse(grammarYaml);
+    const grammarJson = JSON.stringify(grammar);
+    await Deno.writeFile(
+        `./out/${file.name.slice(0, file.name.lastIndexOf("."))}.json`,
+        new TextEncoder().encode(grammarJson),
+        {create: true},
+    );
+}
