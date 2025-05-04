dda8bcb2edb40f2ea27a4f7ad9eb57ce0398dfa9
wooningeire <22846982+wooningeire@users.noreply.github.com>
2025-05-04 08:09:37 -0400

    chore: remove more components

--- plover_hatchery/lib/pipes/__init__.py
+++ plover_hatchery/lib/pipes/__init__.py
@@ -4,15 +4,12 @@ Two types of elision:
     - boundary (placing vowel on the transition from right to left consonant chords)
 """
 
-# from .consonants_vowels_enumeration import consonants_vowels_enumeration, ConsonantsVowelsEnumerationHooks
-# from .banks import banks, BanksApi, BanksState
 from .diphthong_transition_consonants import diphthong_transition_consonants
 from .optional_middle_vowels import optional_middle_vowels
 from .optional_middle_consonants import optional_middle_consonants
 from .optional_unstressed_middle_consonants import optional_unstressed_middle_consonants
 from .alt_chords import alt_chords
 # from .linker_chord import linker_chord
-# from .key_by_key_lookup import key_by_key_lookup
 # from .path_traversal_reverse_lookup import path_traversal_reverse_lookup
 # from .intrabank_consonant_inversions import intrabank_consonant_inversions
 from .floating_keys import floating_keys
