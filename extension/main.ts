aff0902b74ab2c3fd0633bbeab4d9083476dca95
wooningeire <22846982+wooningeire@users.noreply.github.com>
2025-06-08 10:00:16 -0400

    perf: optimize tmlanguage

--- extension/main.ts
+++ extension/main.ts
@@ -1,7 +1,24 @@
+import {exists} from "jsr:@std/fs/exists";
 import {parse} from "@eemeli/yaml";
 
-const grammarYaml = new TextDecoder().decode(await Deno.readFile("./syntaxes/hatchery.tmLanguage.yaml"));
-const grammar = parse(grammarYaml);
-const grammarJson = JSON.stringify(grammar);
-await Deno.writeFile("./out/hatchery.tmLanguage.json", new TextEncoder().encode(grammarJson));
+if (await exists(`./out/`)) {
+    await Deno.remove(`./out/`, { recursive: true });
+}
+await Deno.mkdir(`./out/`, { recursive: true });
+
+const promises: Promise<void>[] = [];
+
+for await (const entry of Deno.readDir("./syntaxes/")) {
+    if (!entry.name.endsWith(".yaml")) continue;
+
+    promises.push((async () => {
+        const filename = entry.name.slice(0, -5);
+        const grammarYaml = new TextDecoder().decode(await Deno.readFile(`./syntaxes/${filename}.yaml`));
+        const grammar = parse(grammarYaml);
+        const grammarJson = JSON.stringify(grammar);
+        await Deno.writeFile(`./out/${filename}.json`, new TextEncoder().encode(grammarJson));
+    })());
+}
+
+await Promise.all(promises);
 
