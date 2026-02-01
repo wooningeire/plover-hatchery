97c83875fc4167219b30b81986183a3a3700b31b
wooningeire <22846982+wooningeire@users.noreply.github.com>
2026-01-31 22:24:39 -0500

    fix: capital chords, filtering empty strokes

--- plover_hatchery/lib/pipes/amphitheory_outlines.py
+++ plover_hatchery/lib/pipes/amphitheory_outlines.py
@@ -6,6 +6,9 @@ from plover_hatchery.lib.pipes.Plugin import GetPluginApi, Plugin, define_plugin
 
 def amphitheory_outlines() -> Plugin[None]:
     linker_chord = Stroke.from_steno("^")
+    capital_chord = Stroke.from_steno("#")
+
+    modifiers_stroke = linker_chord + capital_chord
 
     @define_plugin(amphitheory_outlines)
     def plugin(get_plugin_api: GetPluginApi, **_):
@@ -15,6 +18,7 @@ def amphitheory_outlines() -> Plugin[None]:
         @dataclass
         class AmphitheoryOutlinesState:
             link: bool = False
+            capital: bool = False
 
 
         @soph_trie_api.begin_lookup.listen(amphitheory_outlines)
@@ -24,11 +28,17 @@ def amphitheory_outlines() -> Plugin[None]:
 
         @soph_trie_api.process_outline.listen(amphitheory_outlines)
         def _(state: AmphitheoryOutlinesState, outline: tuple[Stroke, ...], **_):
+            # The empty outline is not allowed
             if len(outline) == 0:
                 return None
             
 
             for i, stroke in enumerate(outline):
+                # Strokes that are only modifiers are not allowed
+                if stroke in modifiers_stroke:
+                    return None
+
+                # All strokes after the first must contain the linker chord
                 if i == 0: continue
 
                 if linker_chord not in stroke:
@@ -38,14 +48,23 @@ def amphitheory_outlines() -> Plugin[None]:
             if linker_chord in outline[0]:
                 state.link = True
 
+            if capital_chord in outline[0]:
+                state.capital = True
+                
 
-            return tuple(stroke - linker_chord for stroke in outline)
+            return tuple(stroke - linker_chord - capital_chord for stroke in outline)
 
 
         @soph_trie_api.modify_translation.listen(amphitheory_outlines)
         def _(state: AmphitheoryOutlinesState, translation: str, **_):
-            if state.link:
+            if state.link and state.capital:
+                return "{^~|}" + translation
+
+            elif state.link:
                 return "{^}" + translation
+            
+            elif state.capital:
+                return "{~|}" + translation
             
             return translation
 
