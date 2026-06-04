3a17a31453412ba67d9dc529448b96e53e2a195a
wooningeire <22846982+wooningeire@users.noreply.github.com>
2026-06-04 02:52:12 -0700

    feat: add maturin dev option to plover_debug local util

--- local-utils/plover_debug.py
+++ local-utils/plover_debug.py
@@ -4,6 +4,7 @@ from pathlib import Path
 import time
 
 from _shared import DEFAULT_PLOVER_PATH_STR
+from maturin_dev import _main as _maturin_dev_main
 
 def _main(args: argparse.Namespace):
     os.chdir(args.plover_path)
@@ -13,6 +14,9 @@ def _main(args: argparse.Namespace):
         raise Exception
     
     time.sleep(2)
+
+    if args.maturin_dev:
+        _maturin_dev_main(args, [])
 
     if args.reinstall:
         exit_code = os.system(fr"""plover_console -s plover_plugins install -e {Path(__file__).parent.parent}""")
@@ -29,6 +33,7 @@ def _main(args: argparse.Namespace):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     _ = parser.add_argument("--plover-path", help="Path to the directory that directly contains Plover's python_console binary", default=DEFAULT_PLOVER_PATH_STR)
+    _ = parser.add_argument("--maturin-dev", "-m", action="store_true", help="Run maturin_dev.py before reinstalling the plugin")
     _ = parser.add_argument("--reinstall", "-r", action="store_true", help="Reinstall the plugin")
     args = parser.parse_args()
     
